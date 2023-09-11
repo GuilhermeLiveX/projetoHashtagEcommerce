92a81b10c71dea596cfc20e6076a9f63a87e0cf2
Guilherme Santana <guilhermehss17@gmail.com>
2023-09-11 17:15:10 -0400

    order, order history and checkout completed

--- checkout.js
+++ checkout.js
@@ -1,10 +1,37 @@
-import { desenharProdutosCarrinhoSimples, lerLocalStorage } from "./src/utilidades";
+import { apagarDoLocalStorage, desenharProdutosCarrinhoSimples, lerLocalStorage, salvarLocalStorage } from "./src/utilidades";
+import { atualizarPrecoCarrinho } from "./src/menuCarrinho";
 
 function desenharProdutosCheckout() {
-    const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho");
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {};
     for (const idProduto in idsProdutoCarrinhoComQuantidade) {
-        desenharProdutosCarrinhoSimples(idProduto, "container-produto-checkout", idsProdutoCarrinhoComQuantidade[idProduto]);
+        desenharProdutosCarrinhoSimples(idProduto, "container-produtos-checkout", idsProdutoCarrinhoComQuantidade[idProduto]);
     }
 }
 
+function finalizarCompra(evento) {
+    evento.preventDefault();
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {};
+    if (Object.keys(idsProdutoCarrinhoComQuantidade).length === 0) {
+        return;
+    }
+
+    const dataAtual = new Date();
+    const pedidoFeito = {
+        dataPedido: dataAtual,
+        pedido: idsProdutoCarrinhoComQuantidade
+    }
+
+    const historicoDePedidos = lerLocalStorage("historico") ?? [];
+    const historicoDePedidosAtualizado = [pedidoFeito, ...historicoDePedidos];
+
+    salvarLocalStorage("historico", historicoDePedidosAtualizado)
+    apagarDoLocalStorage("carrinho");
+    
+    window.location.href = window.location.origin + "/pedidos.html";
+}
+
 desenharProdutosCheckout();
+
+document.addEventListener("submit", (evt) => finalizarCompra(evt));
+
+atualizarPrecoCarrinho();
--- src/utilidades.js
+++ src/utilidades.js
@@ -73,13 +73,17 @@ export function lerLocalStorage(chave) {
     return JSON.parse(localStorage.getItem(chave));
 }
 
+export function apagarDoLocalStorage(chave) {
+    localStorage.removeItem(chave);
+}
+
 export function desenharProdutosCarrinhoSimples(idProduto, idContainerHtml, quantidadeProduto) {
 
     const produto = catalogo.find((p) => p.id === idProduto);
     const containerProdutoCarrinho = document.getElementById(idContainerHtml);
     
     const elementoArticle = document.createElement("article");
-    const articleClasses = ["flex", "bg-stone-200", "rounded-lg", "p-1", "relative", "mb-2"];
+    const articleClasses = ["flex", "bg-stone-200", "rounded-lg", "p-1", "relative", "mb-2", "w-96"];
     
     for (const articleClass of articleClasses) {
         elementoArticle.classList.add(articleClass);
